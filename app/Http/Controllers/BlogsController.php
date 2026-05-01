c0ecbc089d91e293da5d6f22ff943ca4b75225a6
Lucas Varela <lucasn.var@gmail.com>
2026-05-01 18:03:45 -0300

    se agrega tabla para vista de blogs

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BlogsController extends Controller
 {
     public function index()
     {
-        return view('blogs.index');
+        $blogs = DB::table('blogs')->get();
+        /* dd($blogs); */
+        return view('blogs.index', [
+            'blogs' => $blogs,
+        ]);
     }
 }
--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,5 +1,33 @@
+<?php
+/* dd($blogs); */
+?>
+
 <x-main-layout>
     <x-slot:title>Blogs</x-slot>
     <h1>VarWoods - Blogs</h1>
     <p>Descubre las últimas noticias y artículos sobre muebles de madera.</p>
+
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>Título</th>
+                <th>Categoría</th>
+                <th>Contenido</th>
+                <th>Resumen</th>
+                <th>Fecha de Publicación</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($blogs as $blog)
+                <tr>
+                    <td>{{ $blog->title }}</td>
+                    <td>{{ $blog->category_name }}</td>
+                    <td>{{ $blog->contenido_blog }}</td>
+                    <td>{{ $blog->resumen }}</td>
+                    <td>{{ $blog->fecha_publicacion }}</td>
+                    <td>comming soon...</td>
+                </tr>
+            @endforeach
+    </table>
 </x-main-layout>
