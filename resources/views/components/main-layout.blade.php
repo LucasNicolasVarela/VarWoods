df41939ee556f549de435909d0a00cbe7c40237a
Lucas Varela <lucasn.var@gmail.com>
2026-06-09 18:52:52 -0300

    se agrega favicon al proyecto

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? '' }} :: VarWoods</title>
+
+    <link rel="icon" href="{{ asset('favicon-varwoods.png') }}" type="image/png">
+
     <link rel="stylesheet" href=" <?= url('css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href=" <?= url('css/style.css'); ?>">
 </head>
