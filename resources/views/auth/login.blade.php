3d5bed820fb0b8fa4490783b8ea4be1f6e7f95bc
Lucas Varela <lucasn.var@gmail.com>
2026-06-23 23:48:58 -0300

    se agregan estilos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,25 @@
 <x-main-layout>
     <x-slot:title>Iniciar Sesión</x-slot:title>
 
-    <h1>Iniciar Sesión</h1>
+    <div class="container py-2">
+        <h1>Iniciar Sesión</h1>
 
-    <form action="{{ route('login.process') }}" method="post">
-        <div class="mb-3">
-            <label for="email" class="form-label">Correo Electrónico</label>
-            <input type="email" class="form-control" id="email" name="email">
-        </div>
-        <div class="mb-3">
-            <label for="password" class="form-label">Contraseña</label>
-            <input type="password" class="form-control" id="password" name="password">
-        </div>
-        <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
-    </form>
+        <form action="{{ route('login.process') }}" method="post">
+            <div class="mb-3">
+                <label for="email" class="form-label">Correo Electrónico</label>
+                <input type="email" class="form-control" id="email" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Contraseña</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+        </form>
 
-    <p class="mt-3">
-        ¿No tienes cuenta?
-        <a href="{{ route('register') }}">Crear cuenta</a>
-    </p>
+        <p class="mt-3">
+            ¿No tienes cuenta?
+            <a href="{{ route('register') }}">Crear cuenta</a>
+        </p>
+    </div>
+
 </x-main-layout>
